a5b82c21cfa0b5f6d77b9f068f16f8a7c4080411
Eliav Menachi <eliav.menachi@gmail.com>
2024-11-27 19:13:30 +0200

    jest

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECTION);
+const bodyParser = require("body-parser");
+const postsRoute = require("./routes/posts_route");
+
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const postsRoute = require("./routes/posts_route");
 app.use("/posts", postsRoute);
 
 app.get("/about", (req, res) => {
   res.send("Hello World!");
 });
 
-module.exports = app;
+const initApp = () => {
+  return new Promise(async (resolve, reject) => {
+    await mongoose.connect(process.env.DB_CONNECTION);
+    resolve(app);
+  });
+};
+
+module.exports = initApp;
