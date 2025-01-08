e6393ad7410b76314ab00118da89779445bd105f
Eliav Menachi <eliav.menachi@gmail.com>
2025-01-08 19:05:21 +0200

    cors

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,12 @@ db.once("open", () => console.log("Connected to Database"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
 app.use("/auth", authController);
